e4b344e16886d2cabac61eab39846ae4cb7e1eef
k.ayupov <ayupov.k@romir.ru>
2026-02-01 22:55:58 +0300

    Finished

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -4,12 +4,39 @@ import (
 	"fmt"
 )
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+    russian = "Russian"
+    spanish = "Spanish"
+    french = "French"
+    russianHelloPrefix = "Привет, "
+    frenchHelloPrefix = "Bonjour, "
+    englishHelloPrefix = "Hello, "
+    spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case spanish:
+        prefix = spanishHelloPrefix
+    case french:
+        prefix = frenchHelloPrefix
+    case russian:
+        prefix = russianHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return
 }
 
 func main() {
-  hello := Hello("Chris chan")
-
+    hello := Hello("Chris chan", "English")
 	fmt.Println(hello)
 }
